4d3dfd578c7f3d42bab5b93adb6d4c77ae33428a
fulldev319 <fulldev326@gmail.com>
2020-02-02 11:33:52 -0800

    initial commit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React, { useState, useEffect } from "react";
+import Grid from "@material-ui/core/Grid";
+import PplCard from "./PplCard";
+import "./App.css";
+
+function App() {
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    async function fetchData() {
+      setUsers(
+        await fetch("https://reqres.in/api/users?page=2")
+          .then(res => res.json())
+          .then(res => res.data)
+          .catch(err => console.log("Fetching warning!!!", err))
+      );
+    }
+    fetchData();
+  }, []);
+
+  return (
+    <div className="App">
+      <h3>THE TRUE BEAUTY OF MATERIAL UI</h3>
+      <Grid container spacing={10} style={{ padding: "24px" }}>
+        {users.map(user => (
+          <Grid item xs={12} sm={6} md={4} lg={4} xl={3} key={user.id}>
+            <PplCard user={user} />
+          </Grid>
+        ))}
+      </Grid>
+    </div>
+  );
+}
+
+export default App;
